d6a59ce8300d79cb7215de5604b1047f389cec47
SOLV-Code <gottfries.pestal@solv.ca>
2020-04-27 08:42:01 -0700

    modified calcMonthly()

--- R/FUNCTION_calcMonthly.R
+++ R/FUNCTION_calcMonthly.R
@@ -1,19 +1,20 @@
 #' calcMonthly
 #'
-#' Calculate monthly split of annual mortality rate
-#' @param stk = text string with stock name (e.g. "ATN")
-#' @param fmap = data frame with fisheries mapping
+#' Calculate monthly distribution of tag recoveries for custom fishery groupings (loops through all stocks in the CAS input file).
+#' @param fmap = data frame that matches 195 CAS fishery ids (CFILENO) in col 1 to a lookup grouping in col 2. Lookup groupings are used as output labels.
 #' @param pathdb = text string with path to the CAS database (need either this or the cas input below)
-#' @param cas = data frame with CAS data, generated from databases (need either this or the pathdb input above)
-#' @param pathcmz = text string with path to CMZ file
+#' @param cas = data frame with CAS data, generated from database (need either this or the pathdb input above)
 #' @param tracing = if TRUE, print various summaries  
+#' @param infill = if TRUE, include months with 0 recoveries
 #' @export
 #' @examples
-#' 
+#'
 
 
-calcMonthly <- function(stk = "ATN", fmap = NULL,pathdb = NULL,cas = NULL, pathcmz = NULL,
-                        tracing = FALSE){
+calcMonthly <- function(fmap = NULL, 
+                        pathdb = NULL,cas = NULL,
+                        tracing = FALSE,
+                        infill = TRUE){
 
 
   
@@ -21,42 +22,124 @@ calcMonthly <- function(stk = "ATN", fmap = NULL,pathdb = NULL,cas = NULL, pathc
 if(is.null(fmap)){warning("Need a fisheries mapping input!"); stop()}
 if(is.null(pathdb) & is.null(cas)){warning("Need either a path to the CAS DB, or a dataframe input"); stop()} 
 if(!is.null(pathdb) & !is.null(cas)){warning("Cannot have both a path to the CAS DB and a dataframe input. Pick one!"); stop()}
-if(is.null(pathcmz)){warning("Need a CMZ input!"); stop()}   
-   
+#if(is.null(pathcmz)){warning("Need a CMZ input!"); stop()}   
+  
 if(!is.null(pathdb)){casdf <- readcasdbsplit(pathdb)}
 if(!is.null(cas)){casdf <- cas}
+ 
+#if(!(stk %in% unique(casdf$Stock))){warning("Selected 'stk' is not in the CAS data."); stop()}     
+   
+#cmz <- readcmzsplit(pathcmz)
+#casdf$nameCMZ <- fmap$nameCMZ[match(casdf$CFileFishery_Id, fmap$CFILENO)]
+
+monthlycwt.out <- data.frame(Stock = NA,
+                               Stock_Name = NA,
+                               Fishery_Group = NA,
+                               Recovery_Year = NA,
+                               Recovery_Month = NA,
+                               monthtotal = NA,
+                               yeartotal = NA,
+                               monprop = NA)
   
-cmz <- readcmzsplit(pathcmz)
-casstk <- unique(casdf$Stock)  
-casdf$nameCMZ <- fmap$nameCMZ[match(casdf$CFileFishery_Id, fmap$CFILENO)]
+  
+  
+stk.list <- sort(unique(casdf$Stock))
+print(stk.list)
 
-dbstk <- casdf[casdf$Stock==stk,]
+fgroups.list <- sort(unique(fmap[,2]))  
+print(fgroups.list )
+  
+for(stk in stk.list){
+  
+  print("#######################################")
+  print(stk)
+  print("#######################################")
 
-monthlycwt <- aggregate(dbstk$Total_Expanded, 
-                        list(Stock=dbstk$Stock, Stock_Name=dbstk$Stock_Name, 
-                                        Recovery_Year=dbstk$Recovery_Year,  
-                                        Recovery_Month=dbstk$Recovery_Month), sum) %>%
+for(fgroup in fgroups.list){
+
+print("---")
+print(fgroup)
+
+fgroup.ids <- fmap$CFILENO[fmap[,2] == fgroup]
+#print(fgroup.ids)
+
+db.sub  <- casdf %>% dplyr::filter(Stock==stk, CFileFishery_Id %in% fgroup.ids)
+
+stk.name <- unique(db.sub$Stock_Name)
+#print(dim(db.sub))
+
+
+
+if(dim(db.sub)[1]>0){
+  
+monthlycwt <- aggregate(db.sub$Total_Expanded, 
+                        list( Stock=db.sub$Stock, Stock_Name=db.sub$Stock_Name, 
+                              Fishery_Group = rep(fgroup,dim(db.sub)[1]), 
+                                        Recovery_Year=db.sub$Recovery_Year,  
+                                        Recovery_Month=db.sub$Recovery_Month), sum) %>%
                         rename(monthtotal  = x)
-yearlycwt <- aggregate(dbstk$Total_Expanded, list(Stock=dbstk$Stock,Stock_Name= dbstk$Stock_Name,  Recovery_Year=dbstk$Recovery_Year), sum)
+yearlycwt <- aggregate(db.sub$Total_Expanded, list(Stock=db.sub$Stock,Stock_Name= db.sub$Stock_Name,  Recovery_Year=db.sub$Recovery_Year), sum)
 
 monthlycwt$yeartotal <- yearlycwt$x[match(monthlycwt$Recovery_Year,yearlycwt$Recovery_Year )]
 
-#these proportions need to be multiplied by the annual mortality. 
-print(monthlycwt$monthtotal/monthlycwt$yeartotal)
+# monthly proportions
 monthlycwt$monprop <- monthlycwt$monthtotal/monthlycwt$yeartotal 
 
-return(monthlycwt)
+# fill in missing month
+# could probably do this whole loop more elegantly with spread/gather or something.
+if(infill == TRUE){
+for(yr in sort(unique(monthlycwt$Recovery_Year))){
+
+missing.months  <-  setdiff(1:12,monthlycwt$Recovery_Month[monthlycwt$Recovery_Year == yr])
+
+if(length(missing.months) > 0 ){
+add.rows <- data.frame(Stock = stk, Stock_Name = stk.name, Fishery_Group = fgroup,
+                       Recovery_Year = yr,Recovery_Month = missing.months ,
+                       monthtotal = 0 , yeartotal = unique(monthlycwt$yeartotal), 
+					   monprop = 0)
+				   
+					   
+monthlycwt <- rbind(monthlycwt,add.rows)
+} # end if missing months
+
+} # end adding missing month
+} # end if infill = TRUE
+
+monthlycwt.out <-  rbind(monthlycwt.out,monthlycwt)
+
+#print("a------------")
+#print(add.rows)
+#print("m------------")
+#print(monthlycwt)
+#print("o------------")
+#print(monthlycwt.out)
+
+} # end if dim[1] > 0
+
+
+} # end looping through fisheries
+} # end looping through stocks
+
+# drop blank row
+monthlycwt.out <- monthlycwt.out[-1,]
+  
+# sort the output by Fishery/Year/Month
+monthlycwt.out <- monthlycwt.out %>% arrange(Stock,Fishery_Group, Recovery_Year,Recovery_Month)
+
+return(monthlycwt.out)
+
+
   
 if(tracing){
 # need labels etc here
 print("casdf ------------------------")
 summary(casdf)
-summary(cmz)
+#summary(cmz)
 summary(fmap)
 print("Fisheries --------------------")
 unique(casdf$CFileFishery_Name)
 print("--------------------")
-match(fmap$nameCMZ,names(cmz[,4:ncol(cmz)]))
+#match(fmap$nameCMZ,names(cmz[,4:ncol(cmz)]))
 summary(dbstk)
 }
 
